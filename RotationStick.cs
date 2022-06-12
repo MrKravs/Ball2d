2dc373aba841ab7e875f5add4eba0c11a93258be
MrKravs <100081187+MrKravs@users.noreply.github.com>
2022-06-12 16:06:23 +0300

    added levels and audio

---
+++ RotationStick.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationStick : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private int _dirRotaion = 1;
+    private AudioSource audioSource;
+    public AudioClip audioClip;
+    private GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Bullet");
+    }
+    void FixedUpdate()
+    {
+        gameObject.transform.Rotate(new Vector3(0, 0, Time.deltaTime * _dirRotaion * _speed));
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Bullet")
+        {
+            audioSource.PlayOneShot(audioClip);
+        }
+    }
+}
